5472e362f45a5e2655100c66806dd20d19878a82
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-09-14 18:05:13 +0200

    delete old stuff

--- derive-new/src/lib.rs
+++
@@ -1,6 +0,0 @@
-static MACRO: watt::WasmMacro = watt::WasmMacro::new(WASM);
-static WASM: &[u8] = include_bytes!("derive-new.wasm");
-#[proc_macro_derive(new, attributes(new))]
-pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    MACRO.proc_macro_derive(stringify!(derive), input)
-}
